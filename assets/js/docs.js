f47f8235dd521804adffb57b31ec5fa20321e46d
Eric Arenson <eric.arenson@portlandoregon.gov>
2012-03-28 08:15:10 -0700

    First commit

---
+++ assets/js/docs.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    
+    console.log("Sisk boom pow");
+
+    $('.well h4').each(function(){
+        $(this).wrap(function(){
+            var link = $('<a/>');
+            var href;
+            
+            switch($(this).text()){
+            
+                case 'Understanding PDX CitySync' :
+                    href = 'understanding-pdx-citysync';
+                    break;
+                case 'Getting Started' :
+                    href = 'getting-started';
+                    break;
+                case 'Building & Submitting Your App':
+                    href = 'building-and-submitting';
+                    break;
+                case 'JavaScript SDK':
+                    href = 'javascript-sdk';
+                    break;
+                default :
+                    href = 'sample-app';
+                    break;
+            }
+            
+            
+            link.attr('href', '../' + href);
+            return link;     
+        });
+        
+
+    });
+
+});
